f9bc6a35a1258f4317906d713d7668b9573f28d2
skorp-dev <skorpwebx@gmail.com>
2023-01-28 16:25:43 +0100

    download users from server and set users cards

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import User from "../User";
+import s from "./style.module.css";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch("https://dummyjson.com/users?limit=15");
+      const data = await resp.json();
+
+      setUsers(
+        data.users.map(({ id, firstName, lastName, gender, email, image }) => ({
+          id,
+          firstName,
+          lastName,
+          email,
+          gender,
+          image,
+        }))
+      );
+    })();
+  }, []);
+
+  const deleteUser = (id) => {
+    setUsers(users.filter((product) => product.id !== id));
+  };
+
   return (
-    <div>App</div>
-  )
+    <div className={s.container}>
+      {users.map((user) => (
+        <User key={user.id} {...user} deleteUser={deleteUser} />
+      ))}
+    </div>
+  );
 }
--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -3,8 +3,8 @@ import s from "./style.module.css";
 
 export default function User({
   id,
-  firstname,
-  lastname,
+  firstName,
+  lastName,
   gender,
   email,
   image,
@@ -12,9 +12,9 @@ export default function User({
 }) {
   return (
     <div className={[s.card, gender === "male" ? s.male : s.female].join(" ")}>
-      <img src={image} alt={firstname} />
-      <p>{firstname}</p>
-      <p>{lastname}</p>
+      <img src={image} alt={firstName} />
+      <p>{firstName}</p>
+      <p>{lastName}</p>
       <a href={email}>{email}</a>
       <button className={s.del_user} onClick={() => deleteUser(id)}></button>
     </div>
